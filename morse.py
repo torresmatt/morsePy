5685d09d0819dede8531818db111e2dc141431dc
Matt Torres <torresmatt@gmail.com>
2015-10-18 21:33:30 -0500

    Filemode is done!

--- morse.py
+++ morse.py
@@ -60,12 +60,39 @@ def morseString(original):
 		#if it's a digit or a letter, add the morse code dot sequence then a divider "|"
 		elif c.isdigit() or c.isalpha():
 			result += morseChar(c) + "|"
+		# if endline, put that too
+		elif c == '\n':
+			result += c
 	return result
 
 def fileMode():
+	# if there aren't 4 arguments, there aren't enough for filemode. print message and exit
 	if (len(sys.argv) < 4):
-		print "Not enough arguments!"
-		print helpMenu()
+		print("Two filenames must be provided after -f")
+		print(helpMenu())
+		quit()
+	# assign parameters for infile and outfile to variables
+	inFileName = sys.argv[2]
+	outFileName = sys.argv[3]
+
+	#open files
+	print("Opening input file " + inFileName)
+	inFile = open(inFileName, 'r')
+	print("Opening output file " + outFileName)
+	outFile = open(outFileName, 'w')
+
+	#write the translation of the input file line by line to output file
+	print("Translating and writing to " + outFileName + "...")
+	for line in inFile:
+		outFile.write(morseString(line.upper()) + "\n")
+	print("Done writing")
+
+	#close files
+	inFile.close()
+	outFile.close()
+
+
+
 
 def inputMode():
 	print(morseString(raw_input("Enter phrase: ").upper()))
@@ -102,7 +129,7 @@ elif (len(sys.argv) > 1):
 	# if first param is not -f, just do the rest as a phrase
 	else:
 		phrase = ' '.join(sys.argv[1:]).upper()
-		print morseString(phrase)
+		print(morseString(phrase))
 else:
 	print(helpMenu())
 
